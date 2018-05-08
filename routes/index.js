a6dc67b58279192b943d02c3bfbeb818f027a7c3
Biodun Chris <biodunch@gmail.com>
2018-05-08 09:53:15 +0100

    add relevant details

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET Service page. */
+router.get('/service', function(req, res, next) {
+  res.render('services', { title: 'Service' });
+});
+
 
 module.exports = router;
